d684b4e41f374128bea0093b16e392c4d99d7cca
niels <kormarun@gmail.com>
2023-04-30 17:50:17 +0200

    caching

--- src/main/java/de/noname/pizza4c/Pizza4cConfig.java
+++ src/main/java/de/noname/pizza4c/Pizza4cConfig.java
@@ -3,11 +3,13 @@ package de.noname.pizza4c;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class Pizza4cConfig implements WebMvcConfigurer {
@@ -15,12 +17,13 @@ public class Pizza4cConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(7, TimeUnit.DAYS))
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
                         if (resourcePath.contains("/api/")) {
-                            throw new IOException("nooooo");
+                            throw new IOException("Api entries should not be resolved as static addresses.");
                         }
                         Resource requestedResource = location.createRelative(resourcePath);
                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
