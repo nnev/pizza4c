4bd39eef7fca4ab431a7397b60685702ee8aedaf
kormarun <kormarun@gmail.com>
2023-05-05 19:42:27 +0200

    fix crash on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 merge into Known_Restaurant
-    (version, human_Readable_Name, lieferando_Name)
+    (id, version, human_Readable_Name, lieferando_Name)
 KEY(lieferando_Name)
 VALUES
-    (1, 'Pizza Rapido', 'pizza-rapido-eppelheim'),
-    (2, 'Pizza Bananana', '3Q3N1P2');
+    (1, 1, 'Pizza Rapido', 'pizza-rapido-eppelheim'),
+    (2, 1, 'Pizza Bananana', '3Q3N1P2');
 -- select 1;
