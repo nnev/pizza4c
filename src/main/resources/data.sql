57d629e9c27c85c10eae8c4fe1d64157d4d17275
niels <kormarun@gmail.com>
2023-05-18 15:40:27 +0200

    update initial database values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,4 @@ merge into Known_Restaurant
     (id, version, human_Readable_Name, lieferando_Name)
 KEY(lieferando_Name)
 VALUES
-    (1, 1, 'Pizza Rapido', 'pizza-rapido-eppelheim'),
-    (2, 1, 'Pizza Bananana', '3Q3N1P2');
--- select 1;
+    (1, 1, 'Pizza Rapido', 'pizza-rapido-eppelheim');
